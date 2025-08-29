d2e25bd29ade26a73813d9cdd34e012a9f729248
Devansh Sabharwal <sabharwaldevansh04@gmail.com>
2025-08-29 23:02:55 +0530

    create tables on start

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 #psycopg2- postgresql driver
 from fastapi import FastAPI, Depends    
-from app.database.connection import get_db
+from app.database.connection import get_db,engine, Base
 from sqlalchemy.orm import Session
 from app.api.routes import router
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root(db: Session = Depends(get_db)):
